3564205226abdbe875910abadfbb9c5892a6dc9b
Kolesnikova-ekaterina <82054029+Kolesnikova-ekaterina@users.noreply.github.com>
2022-10-23 15:50:35 +0300

    fixed unexpected exeption

--- lib/battleship/field.rb
+++ lib/battleship/field.rb
@@ -73,7 +73,7 @@ class Field
     length = 0
     length = (m['x1'].to_i - m['x2'].to_i ).abs+1 if m['y1']==m['y2']
     length = (to_int(m['y1']) - to_int( m['y2']) ).abs+1 if m['x1']==m['x2']
-    if length>4 || @available_ships[length]==0
+    if length>4 || @available_ships[length-1]==0
       throw InvalidShip
     end
 
