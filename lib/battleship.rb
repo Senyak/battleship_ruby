c04bc98c466f0963472e9aabbd0a7d90d56547d8
Senyak <mselina@sfedu.ru>
2022-10-23 03:19:55 +0300

    Changing some description

--- lib/battleship.rb
+++ lib/battleship.rb
@@ -7,13 +7,27 @@ class Error < StandardError; end
 
 extend Inf
 
+puts "Welcome to the Battleship! (Russian version)"
+puts "For read rules use 'rules'"
+puts "For read some information about game process use 'help'"
+puts "For starting game use 'start'"
+
+
 def start
   f = Field.new
-  puts "For enter your ship use 'add_ship(range)', where range in format 'A2:A4', follow the rules"
+  puts "First, place your ships on the field, follow the rules"
+  puts "  "
 
+  while f.available_ships_quantity > 0
+    f.available_ships_getter
+    p "Enter your ship, where range in format 'A2:A4' (for single-deck range A1:A1):"
+    input = gets.chomp
+    f.add_ship(input)
+    f.show_field
+  end
+
+
+  puts "Finally you placed your ships!"
 end
 
 
-  puts "Welcome to the Battleship! (Russian version)"
-  puts "For read rules use 'rules'"
-  puts "For starting rules use 'start'"
